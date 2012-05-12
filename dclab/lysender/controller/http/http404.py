3a2849515815953e1a744807f52e1693d3d4017e
Lysender <dc.eros@gmail.com>
2012-05-12 16:05:59 +0800

    Updated 404 handling/template

--- dclab/lysender/controller/http/http404.py
+++ dclab/lysender/controller/http/http404.py
@@ -1,7 +1,7 @@
 from dclab.lysender.controller import BaseController
 
 class Http404Controller(BaseController):
-    template_dir = 'http404'
+    template_dir = 'http/http404'
 
     def action_index(self):
         self.template = 'index.html'
